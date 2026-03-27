914064a87df4dcea54e912e1af3e8d609a506490
pranjalmehta12126 <pranjalmehta1907@gmail.com>
2026-03-27 16:46:25 +0530

    Fix SplitText duplicate text issue

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -3,34 +3,32 @@ import "./styles/Landing.css";
 
 const Landing = ({ children }: PropsWithChildren) => {
   return (
-    <div className="landing-section" id="landingDiv">
-      <div className="landing-container">
-        <div className="landing-intro">
-          <h2>Hello! I'm</h2>
-          <h1>
-            PRANJAL
-            <br />
-            <span>MEHTA</span>
-          </h1>
-        </div>
-
-        <div className="landing-info">
-          <h3>A Creative</h3>
-
-          <div className="landing-role-wrap">
-            <div className="landing-info-h2 landing-h2-1">3D Artist</div>
-            <div className="landing-info-h2 landing-h2-2">Video Editor</div>
+    <>
+      <div className="landing-section" id="landingDiv">
+        <div className="landing-container">
+          <div className="landing-intro">
+            <h2>Hello! I'm</h2>
+            <h1>
+              PRANJAL
+              <br />
+              <span>MEHTA</span>
+            </h1>
           </div>
-
-          <div className="landing-role-wrap-2">
-            <div className="landing-h2-info">Video Editor</div>
-            <div className="landing-h2-info-1">3D Artist</div>
+          <div className="landing-info">
+            <h3>A Creative</h3>
+            <h2 className="landing-info-h2">
+              <div className="landing-h2-1">3D Artist</div>
+              <div className="landing-h2-2">Video Editor</div>
+            </h2>
+            <h2>
+              <div className="landing-h2-info">Video Editor</div>
+              <div className="landing-h2-info-1">3D Artist</div>
+            </h2>
           </div>
         </div>
+        {children}
       </div>
-
-      {children}
-    </div>
+    </>
   );
 };
 
